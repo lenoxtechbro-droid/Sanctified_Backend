a961ed814799e9c29e1289eb1044d1292a78c94e
Lenox <lenoxtechbro@gmail.com>
2026-03-07 21:15:46 +0300

    Main.py / noise removed

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ settings = get_settings()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_url, "https://sanctified-frontend.vercel.app/"],
+    allow_origins=[settings.frontend_url, "https://sanctified-frontend.vercel.app", "http://localhost:5173", "http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
